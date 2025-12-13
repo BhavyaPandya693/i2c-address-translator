5981c8403f5169aaa9ce153d198678530d194ef7
Bhavya Pandya <bhavyapandya862@gmail.com>
2025-12-13 18:13:36 +0530

    Update i2c_master.v

--- rtl/i2c_master.v
+++ rtl/i2c_master.v
@@ -1,3 +1,4 @@
+///reset, scl and sda are global signals
 //master module-------------------------------------------------------
 module i2c_master (
   input  wire clk,
@@ -12,7 +13,7 @@ module i2c_master (
   output reg  [7:0] rd_data,//8bit read data
   output reg busy,// busy=0(idle), busy=1(transfer in progress)
   output reg done,// done goes from 0 to 1 when one click cycle is finished and returns back to 0
-  output reg ACK_error,//acknowledgement error
+  output reg ack_error,//acknowledgement error
 
   //data & clock bus
   output wire scl,// serial clock
